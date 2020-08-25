14d228636c9a7ffa42053e2187c06a69133e5db0
alexandreGvozdenovic <alexandre.gvozdenovic@gmail.com>
2020-08-25 15:38:17 +0200

    updated projects part

--- src/components/thirdpage.js
+++ src/components/thirdpage.js
@@ -18,7 +18,12 @@ function ThirdPage() {
                 <Card className='card-container'>
                     <Card.Img src={require('../style/shakengo.jpg')} alt='Kitty' height='256px' />
                     <Card.ImgOverlay className='d-flex flex-column justify-content-end card-overlay'>
-                        <Card.Title><h4 className='card-title'>Shake'N'Go<i className="fab fa-github card-title-icons"></i></h4></Card.Title>
+                        <Card.Title>
+                            <h4 className='card-title'>
+                                Shake'N'Go
+                                <a href='https://github.com/alexandreGvozdenovic/sng-frontend' target="_blank" rel="noopener noreferrer" style={{color: 'white'}}><i className="fab fa-github card-title-icons"></i></a>
+                            </h4>
+                        </Card.Title>
                         <Card.Text className='card-text'>
                             Application mobile qui propose des choix de sorties aléatoires à Paris grâce à Google Places API.<br/>
                         <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
@@ -39,17 +44,27 @@ function ThirdPage() {
             </Col>
             <Col xs={12} md={4} className='col-horizontal-scroll'>
                 <Card className='card-container'>
-                    <Card.Img src={require('../style/Kitty.jpg')} alt='Kitty' height='256px' />
+                    <Card.Img src={require('../style/faceup.jpg')} alt='Kitty' height='256px' />
                     <Card.ImgOverlay className='d-flex flex-column justify-content-end card-overlay'>
-                        <Card.Title><h4 className='card-title'>Nom du projet <i className="fab fa-github card-title-icons"></i></h4></Card.Title>
+                        <Card.Title>
+                            <h4 className='card-title'>
+                                Face Up 
+                                <a href='https://github.com/alexandreGvozdenovic/faceUpFrontend' target="_blank" rel="noopener noreferrer" style={{color: 'white'}}><i className="fab fa-github card-title-icons"></i></a>
+                            </h4>
+                        </Card.Title>
                         <Card.Text className='card-text'>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.<br/>
+                            Application mobile qui utilise la reconnaissance faciale pour donner des informations relative à un visage.<br/>
                         <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
-                            <span className='badge-text'>Techno 1</span>
+                            <span className='badge-text'>React Native</span>
                         </Badge>
                         <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
-                            <span className='badge-text'>Techno 2</span>
+                            <span className='badge-text'>Express.js</span>
+                        </Badge>
+                        <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
+                            <span className='badge-text'>Azure Faces</span>
+                        </Badge>
+                        <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
+                            <span className='badge-text'>Cloudinary</span>
                         </Badge>
                         </Card.Text>
                     </Card.ImgOverlay>
@@ -59,21 +74,35 @@ function ThirdPage() {
                 <Card className='card-container'>
                     <Card.Img src={require('../style/Kitten.png')} alt='Kitty' height='256px' />
                     <Card.ImgOverlay className='d-flex flex-column justify-content-end card-overlay'>
-                        <Card.Title><h4 className='card-title'>Nom du projet <i className="fab fa-github card-title-icons"></i></h4></Card.Title>
+                        <Card.Title>
+                            <h4 className='card-title'>
+                                Kitty Shop 
+                                <a href='https://github.com/alexandreGvozdenovic/kittenShop' target="_blank" rel="noopener noreferrer" style={{color: 'white'}}><i className="fab fa-github card-title-icons"></i></a>
+                                <a href='https://kittyshop.herokuapp.com/' target="_blank" rel="noopener noreferrer" style={{color: 'white'}}><i className="fas fa-globe card-title-icons-rightest"></i></a>
+                            </h4>
+                        </Card.Title>
                         <Card.Text className='card-text'>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.<br/>
+                            E-Shop avec des chatons créé avec l'ensemble des compétences que j'ai acquis durant ma formation.<br/>
                         <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
-                            <span className='badge-text'>Techno 1</span>
+                            <span className='badge-text'>React</span>
                         </Badge>
                         <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
-                            <span className='badge-text'>Techno 2</span>
+                            <span className='badge-text'>Express.js</span>
+                        </Badge>
+                        <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
+                            <span className='badge-text'>Redux</span>
+                        </Badge>
+                        <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
+                            <span className='badge-text'>Mongo DB</span>
+                        </Badge>
+                        <Badge className='d-inline-flex align-items-center justify-content-center badge' pill >
+                            <span className='badge-text'>Stripe</span>
                         </Badge>
                         </Card.Text>
                     </Card.ImgOverlay>
                 </Card>
             </Col>
-            <Col xs={12} md={4} className='col-horizontal-scroll'>
+            {/* <Col xs={12} md={4} className='col-horizontal-scroll'>
                 <Card className='card-container'>
                     <Card.Img src={require('../style/Kitty.jpg')} alt='Kitty' height='256px' />
                     <Card.ImgOverlay className='d-flex flex-column justify-content-end card-overlay'>
@@ -90,7 +119,7 @@ function ThirdPage() {
                         </Card.Text>
                     </Card.ImgOverlay>
                 </Card>
-            </Col>
+            </Col> */}
             </Row>
             </Container>
         <Row className='footer-container' id='contact'>
